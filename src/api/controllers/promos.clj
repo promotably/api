4d8c99c52f6dcab27c10adbf8fa8231e27cde994
Thomas Steffes <smnirven@gmail.com>
2014-06-19 22:38:20 -0400

    Re-enabling schema coercion in promo endpoints
    Signed-off-by: Thomas Steffes <smnirven@gmail.com>

--- src/api/controllers/promos.clj
+++ src/api/controllers/promos.clj
@@ -36,48 +36,48 @@
         (do
           {:body (shape-promo the-promo)})))))
 
-
-;;  {:site-id "26b28c70-2144-4427-aee3-b51031b08426", :applied-coupons ["twentyoff"], :code "twentyoff", :shopper-email "cvillecsteele@gmail.com", :shopper-id nil, :cart-contents [{:line-total 39.98, :line-subtotal 39.98, :variation "", :variation-id "", :product-categories [13], :line-subtotal_tax 0, :line-tax 0, :quantity 2, :product-id 11}], :product-ids-on-sale []}
-;;
-;; TODO: this schema needs to match above real-world input...
-;;
-
-(let [inbound-schema {(s/required-key :site-id) s/Str
+(let [inbound-schema {(s/required-key :site-id) s/Uuid
                       (s/required-key :code) s/Str
                       (s/required-key :shopper-id) s/Str
-                      (s/required-key :shopper-email) s/Str}]
+                      (s/required-key :shopper-email) s/Str
+                      (s/optional-key :applied-coupons) [s/Str]
+                      (s/optional-key :cart-items) [{(s/required-key :product-id) s/Str
+                                                     (s/optional-key :product-title) s/Str
+                                                     (s/optional-key :product-type) s/Str
+                                                     (s/optional-key :product-categories) [s/Str]
+                                                     (s/optional-key :variation-id) s/Str
+                                                     (s/optional-key :variation) s/Str
+                                                     (s/optional-key :quantity) s/Int
+                                                     (s/optional-key :line-total) s/Num
+                                                     (s/optional-key :line-subtotal) s/Num
+                                                     (s/optional-key :line-tax) s/Num
+                                                     (s/optional-key :line-subtotal-tax) s/Num}]
+                      (s/optional-key :product-ids-on-sale) [s/Str]}]
   (defn validate-promo
     [{:keys [params body] :as request}]
-    (let [{:keys [site-id code] :as input} (read-str (slurp body) :key-fn keyword)
-          ;;
-          ;; commented out cuz this schema ain't right and I dunno how to fix it yet
-          ;; ... and wtf does cp stand for?
-          ;;
-          ;; {:keys [site-id code] :as cp}
-          ;; ((c/coercer inbound-schema
-          ;;            (c/first-matcher [custom-matcher
-          ;;                              c/string-coercion-matcher]))
-          ;; input)
-          site-uuid (java.util.UUID/fromString site-id)
-          the-promo (promo/find-by-site-uuid-and-code site-uuid code)]
+    (let [input-json (read-str (slurp body) :key-fn keyword)
+          {:keys [site-id code] :as coerced-params}
+            ((c/coercer inbound-schema
+                        (c/first-matcher [custom-matcher
+                                          c/string-coercion-matcher]))
+             input-json)
+          the-promo (promo/find-by-site-uuid-and-code site-id code)]
       (if-not the-promo
         {:status 404 :body "Can't find that promo"}
-        (let [v (promo/valid? the-promo input)]
+        (let [v (promo/valid? the-promo coerced-params)]
           {:status 201 :body (shape-validate (merge v {:uuid (:uuid the-promo)
-                                                       :code code}))})))))
+                                                       :code code}))}))))
 
-;; TODO: use schema
-;; input looks like:
-;;
-;; {"site-id":"26b28c70-2144-4427-aee3-b51031b08426","applied-coupons":["twentyoff"],"code":"twentyoff","shopper-email":"cvillecsteele@gmail.com","shopper-id":null,"cart-contents":[{"product-id":11,"variation":"","variation-id":"","quantity":2,"line-total":39.98,"line-tax":0,"line-subtotal":39.98,"line-subtotal-tax":0,"product-categories":[13]}],"product-ids-on-sale":[]}
-;;
-(defn calculate-promo
-  [{:keys [params body] :as request}]
-  (let [{:keys [site-id code] :as input} (read-str (slurp body) :key-fn keyword)
-        ;; TODO: use schema
-        site-uuid (java.util.UUID/fromString site-id)
-        the-promo (promo/find-by-site-uuid-and-code site-uuid code)]
-    (if-not the-promo
-      {:status 404 :body "Can't find that promo"}
-      (let [v (promo/valid? the-promo)]
-        {:status 201 :body (shape-calculate (merge v {:discount-amount "5.99"}))}))))
+  (defn calculate-promo
+    [{:keys [params body] :as request}]
+    (let [input-json (read-str (slurp body) :key-fn keyword)
+          {:keys [site-id code] :as coerced-params}
+          ((c/coercer inbound-schema
+                      (c/first-matcher [custom-matcher
+                                        c/string-coercion-matcher]))
+           input-json)
+          the-promo (promo/find-by-site-uuid-and-code site-id code)]
+      (if-not the-promo
+        {:status 404 :body "Can't find that promo"}
+        (let [v (promo/valid? the-promo coerced-params)]
+          {:status 201 :body (shape-calculate (merge v {:discount-amount "5.99"}))})))))
