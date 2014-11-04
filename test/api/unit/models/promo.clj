f2150f936b20c6d2a0ad28fb23a6cc4574832197
Thomas Steffes <tsteffes@roomkey.com>
2014-11-04 11:51:03 -0800

    Fix da build
    Signed-off-by: Thomas Steffes <tsteffes@roomkey.com>

--- test/api/unit/models/promo.clj
+++ test/api/unit/models/promo.clj
@@ -1,37 +1,27 @@
 (ns api.unit.models.promo
   (:require [api.models.promo :refer :all]
             [api.models.redemption :as rd]
-            [clj-time.coerce :refer [to-sql-date]]
+            [clj-time.coerce :refer [to-sql-date to-date]]
             [clj-time.core :refer [now plus minus months]]
             [midje.sweet :refer :all]))
 
 (fact "Inactive promos are declared invalid"
-  (let [the-promo {:active false}]
-    (valid? the-promo anything) => (just {:valid false
-                                          :messages ["That promo is currently inactive"]})))
+  (let [the-promo {:active false}
+        output (valid? the-promo anything)]
+    output => (just [anything ["That promo is currently inactive"]])))
 
-(fact "Promos with a start date in the future are declared invalid"
+(fact "Promos that haven't started yet are declared invalid"
   (let [the-promo {:active true
                    :conditions [{:type :dates
-                                 :start-date (plus (now) (months 1))
-                                 :end-date (plus (now) (months 3))}]}]
-    (valid? the-promo anything) => (just {:valid false
-                                          :messages ["That promo hasn't started yet"]})))
+                                 :start-date (to-date (plus (now) (months 1)))
+                                 :end-date (to-date (plus (now) (months 3)))}]}
+        output (valid? the-promo {})]
+    output => (contains [(contains {:errors (just ["The coupon has expired."])}) anything])))
 
 (fact "Promos that have expired are declared invalid"
   (let [the-promo {:active true
                    :conditions [{:type :dates
-                                 :start-date (minus (now) (months 3))
-                                 :end-date (minus (now) (months 1))}]}]
-    (valid? the-promo anything) => (just {:valid false
-                                          :messages ["That promo has ended"]})))
-
-(fact "Promos with a usage-count condition are validated correctly"
-  (let [the-promo {:active true
-                   :conditions [{:type :usage-count
-                                 :usage-count 100}]}]
-    (valid? the-promo anything) => (just {:valid false
-                                          :messages ["That promo is no longer available"]})
-    (provided (api.models.redemption/count-by-promo-and-shopper-email anything anything) => 101)
-    (valid? the-promo anything) => (just {:valid true})
-    (provided (api.models.redemption/count-by-promo-and-shopper-email anything anything) => 99)))
+                                 :start-date (to-date (minus (now) (months 3)))
+                                 :end-date (to-date (minus (now) (months 1)))}]}
+        output (valid? the-promo {})]
+    output => (contains [(contains {:errors (just ["The coupon has expired."])}) anything])))
