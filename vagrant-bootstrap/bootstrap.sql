01d4c02390ea7708d93848e5952ce1282b0781cc
Colin Steele <cvillecsteele@gmail.com>
2014-11-10 15:55:48 -0500

    Lose vagrant stuff.

--- vagrant-bootstrap/bootstrap.sql
+++
@@ -1,7 +0,0 @@
-CREATE USER p_user WITH PASSWORD 'pr0m0';
-CREATE DATABASE promotably_dev;
-GRANT ALL PRIVILEGES ON DATABASE promotably_dev to p_user;
-\c promotably_dev
-CREATE EXTENSION "uuid-ossp";
-CREATE TABLE migrations(version varchar(255));
-GRANT ALL PRIVILEGES ON TABLE migrations TO p_user;
