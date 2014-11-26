789fd25d2e463c33f99eb3eabc26fa843d37e3e7
Thomas Steffes <tsteffes@promotably.com>
2014-11-25 22:25:07 -0500

    moar promo integration tests
    Signed-off-by: Thomas Steffes <tsteffes@promotably.com>

--- test/api/integration/promos.clj
+++ test/api/integration/promos.clj
@@ -5,6 +5,7 @@
    [api.route :as route]
    [api.core :as core]
    [api.models.site]
+   [api.models.promo :as promo]
    [clojure.tools.logging :as log]
    [clj-http.client :as client]
    [clojure.data.json :as json]
@@ -19,12 +20,20 @@
                      (after :contents
                             (migrate-down))]
 
+  (def site (api.models.site/find-by-name "site-1"))
+  (def site-id (:site-id site))
+  (defn- create-promo
+    [new-promo]
+    (client/post "http://localhost:3000/v1/promos"
+                 {:body (json/write-str new-promo)
+                  :content-type :json
+                  :accept :json
+                  :throw-exceptions false}))
+
   (fact-group :integration
 
               (facts "Promo Create"
-                (let [site (api.models.site/find-by-name "site-1")
-                      site-id (:site-id site)
-                      new-promo {:site-id (str site-id)
+                (let [new-promo {:site-id (str site-id)
                                  :name "Twenty Off"
                                  :code "TWENTYOFF"
                                  :description "You get 20% off. Bitches."
@@ -34,9 +43,43 @@
                                  :reward-applied-to :cart
                                  :exceptions nil
                                  :conditions []}
-                      response (client/post "http://localhost:3000/v1/promos"
-                                            {:body (json/write-str new-promo)
-                                             :headers {"Content-Type" "application/json"
-                                                       "Accept" "application/json"}
-                                             :throw-exceptions false})]
-                  (:status response) => 201))))
+                      r (create-promo new-promo)]
+                  (:status r) => 201))
+
+              (tabular
+               (facts "Promo Create Missing Required Fields"
+                 (let [bc (promo/count-by-site site-id)
+                       np {:site-id (str site-id)
+                           :name "Twenty Off"
+                           :code "TWENTYOFF"
+                           :description "You get 20% off. Bitches."
+                           :reward-amount 20.0
+                           :reward-type :percent
+                           :reward-tax :after-tax
+                           :reward-applied-to :cart
+                           :exceptions nil
+                           :conditions []}
+                       r (create-promo (dissoc np ?remove))
+                       ac (promo/count-by-site site-id)]
+                   (:status r) => 400
+                   ac => bc))
+               ?remove
+               :site-id
+               :name
+               :code
+               :description
+               :reward-amount
+               :reward-type
+               :reward-tax
+               :reward-applied-to
+               :exceptions
+               :conditions)
+
+              (facts "Promo Lookup Happy Path"
+                (let [r (client/get "http://localhost:3000/v1/promos"
+                                    {:query-params {:site-id site-id}
+                                     :content-type :json
+                                     :accept :json
+                                     :throw-exceptions false})
+                      b (json/read-str (:body r))]
+                  (:status r) => 200))))
