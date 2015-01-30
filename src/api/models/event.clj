16c12caf2b44c64c5b24b951ed834f7c8c56aa6c
Brenden Grace <brenden@promotably.com>
2015-01-30 06:51:58 -0500

    Add a function to count a shoppers events by days

--- src/api/models/event.clj
+++ src/api/models/event.clj
@@ -1,5 +1,7 @@
 (ns api.models.event
   (:require [api.entities :refer :all]
+            [clj-time.core :refer [now minus days]]
+            [clj-time.coerce :refer [to-sql-date]]
             [clojure.set :refer [rename-keys]]
             [api.util :refer [hyphenify-key assoc*]]
             [api.lib.schema :refer :all]
@@ -13,9 +15,18 @@
   (let [ks (keys r)]
     (rename-keys r (zipmap ks (map hyphenify-key ks)))))
 
+(sm/defn count-shopper-events-by-days
+  [shopper-uuid :- s/Uuid event-type :- s/Str days-ago :- s/Int]
+  (let [r (select events
+            (aggregate (count :*) :cnt)
+            (where {:shopper_id shopper-uuid
+                    :type event-type
+                    :created_at [>= (to-sql-date (minus (now) (days days-ago)))]}))
+        count (get (first r) :cnt 0)]
+    count))
+
 (sm/defn ^:always-validate find-by-id :- (s/maybe BaseEvent)
          [id :- s/Int]
          (db-to-event
            (first (select events (where {:id id})))))
-
 
