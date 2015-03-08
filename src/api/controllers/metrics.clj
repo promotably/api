9a11f1a71b750b392d366ad2b597569a2a293ab5
Brenden Grace <brenden@promotably.com>
2015-03-08 16:01:58 -0400

    Metrics Lift implemented

--- src/api/controllers/metrics.clj
+++ src/api/controllers/metrics.clj
@@ -99,24 +99,45 @@
                 :average (average-from-rows :revenue-per-visit r)}}]
     {:status 200 :body body}))
 
-(defn get-lift [request]
-  {:status 200
-   :body {"conversion" {"daily" {"promotably" [3.1, 3.31, 3.42, 2.91, 3.09, 3.12, 3.23],
-                                 "control" [2.95, 3.07, 3.11, 2.8, 2.85, 2.97, 3.03]},
-                        "average" {"promotably" 3.17,
-                                   "control" 2.97}},
-          "avg-order-value" {"daily" {"promotably" [37.53, 40.01, 42.11, 45.92, 36.71, 38.05, 40.32],
-                                      "control" [33.54, 37.22, 37.45, 40.11, 31.34, 32.54, 37.34]},
-                             "average" {"promotably" 40.09,
-                                        "control" 35.65}},
-          "abandoned-carts" {"daily" {"promotably" [55.6, 54.34, 56.83, 58.1, 53.21, 50.3, 56.1],
-                                      "control" [60.3, 59.21, 61.2, 63.85, 58.8, 56.2, 63.12]},
-                             "average" {"promotably" 54.93,
-                                        "control" 60.38}},
-          "revenue-per-visit" {"daily" {"promotably" [1.03, 1.10, 1.01, 1.05, 1.11, 1.15, 1.09],
-                                        "control" [0.96, 0.97, 0.91, 0.99, 1.00, 0.92, 0.98]},
-                               "average" {"promotably" 1.08,
-                                          "control" 0.96}}}})
+(defn get-lift
+  [{:keys [params] :as request}]
+  (let [{:keys [site-id start end]} params
+        site-uuid (java.util.UUID/fromString site-id)
+        the-site (site/find-by-site-uuid site-uuid)
+        start-date (convert-date-to-site-tz
+                     (f/parse custom-formatter start) the-site)
+        end-date (convert-date-to-site-tz
+                   (f/parse custom-formatter end) the-site)
+        r (metric/site-lift-by-days site-uuid start-date end-date)
+        body {:total-revenue {
+                :daily {
+                  :inc (list-of-days-from-rows :total-revenue-inc r)
+                  :exc (list-of-days-from-rows :total-revenue-exc r)}
+                :average {
+                  :inc (average-from-rows :total-revenue-inc r)
+                  :exc (average-from-rows :total-revenue-exc r)}}
+              :avg-order-revenue {
+                :daily {
+                  :inc (list-of-days-from-rows :avg-order-revenue-inc r)
+                  :exc (list-of-days-from-rows :avg-order-revenue-exc r)}
+                :average {
+                  :inc (average-from-rows :avg-order-revenue-inc r)
+                  :exc (average-from-rows :avg-order-revenue-exc r)}}
+              :revenue-per-visit {
+                :daily {
+                  :inc (list-of-days-from-rows :revenue-per-visit-inc r)
+                  :exc (list-of-days-from-rows :revenue-per-visit-exc r)}
+                :average {
+                  :inc (average-from-rows :revenue-per-visit-inc r)
+                  :exc (average-from-rows :revenue-per-visit-exc r)}}
+              :order-count {
+                :daily {
+                  :inc (list-of-days-from-rows :order-count-inc r)
+                  :exc (list-of-days-from-rows :order-count-exc r)}
+                :average {
+                  :inc (average-from-rows :order-count-inc r)
+                  :exc (average-from-rows :order-count-exc r)}}}]
+    {:status 200 :body body}))
 
 (defn get-promos
   [{:keys [params] :as request}]
--- test/api/integration/metrics.clj
+++ test/api/integration/metrics.clj
@@ -72,24 +72,36 @@
                       (:status r) => 200))
 
               (fact "Can route to controller.api.metrics.get-lift"
-                    (let [r (request-metrics "/metrics/lift" fix/site-id "20150220" "20150223")
+                    (let [r (request-metrics "/metrics/lift" fix/site-id "20150220" "20150224")
                           b (json/read-str (:body r) :key-fn keyword)]
-                      b => {:conversion {:daily {:promotably [3.1, 3.31, 3.42, 2.91, 3.09, 3.12, 3.23],
-                                                 :control [2.95, 3.07, 3.11, 2.8, 2.85, 2.97, 3.03]},
-                                         :average {:promotably 3.17,
-                                                   :control 2.97}},
-                            :avg-order-value {:daily {:promotably [37.53, 40.01, 42.11, 45.92, 36.71, 38.05, 40.32],
-                                                      :control [33.54, 37.22, 37.45, 40.11, 31.34, 32.54, 37.34]},
-                                              :average {:promotably 40.09,
-                                                        :control 35.65}},
-                            :abandoned-carts {:daily {:promotably [55.6, 54.34, 56.83, 58.1, 53.21, 50.3, 56.1],
-                                                      :control [60.3, 59.21, 61.2, 63.85, 58.8, 56.2, 63.12]},
-                                              :average {:promotably 54.93,
-                                                        :control 60.38}},
-                            :revenue-per-visit {:daily {:promotably [1.03, 1.10, 1.01, 1.05, 1.11, 1.15, 1.09],
-                                                        :control [0.96, 0.97, 0.91, 0.99, 1.00, 0.92, 0.98]},
-                                                :average {:promotably 1.08,
-                                                          :control 0.96}}}
+                      b => {:total-revenue {
+                              :daily {
+                                :inc [4.0 1.0]
+                                :exc [4.0 1.0]}
+                              :average {
+                                :inc 2.5
+                                :exc 2.5}}
+                           :avg-order-revenue {
+                              :daily {
+                                :inc [4.0 1.0]
+                                :exc [4.0 1.0]}
+                              :average {
+                                :inc 2.5
+                                :exc 2.5}}
+                           :revenue-per-visit {
+                              :daily {
+                                :inc [4.0 1.0]
+                                :exc [4.0 1.0]}
+                              :average {
+                                :inc 2.5,
+                                :exc 2.5}}
+                           :order-count {
+                              :daily {
+                                :inc [4 1]
+                                :exc [4 1]}
+                              :average {
+                                :inc 2.5
+                                :exc 2.5}}}
                       (:status r) => 200))
 
               (fact "Can route to controller.api.metrics.get-promos"
